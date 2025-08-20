1cd46329d6ce2fb7385ebc01b26677a94f5ec5e4
Aswin Sreehari <aswinchittethu@gmail.com>
2025-08-21 04:21:01 +0530

    initial commit

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://customer-sentiment-analysis-server.netlify.app/", 
+  baseURL: import.meta.env.VITE_BASE_URL, 
   headers: {
     "Content-Type": "application/json"
   },
